ac158678fe0f6e9fa750e503c567d84552899ca8
Chris Debenham <chris@adebenham.com>
2025-11-15 14:24:40 +1100

    Ensure userid/euserid is set properly

--- updateAccount/updateAccount.php
+++ updateAccount/updateAccount.php
@@ -13,6 +13,14 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+session_name("WishListSite");
+session_start();
+
+if (!isset($_SESSION["userid"])) {
+    header("Location: ../login.php");
+    exit;
+}
+
 include "../funcLib.php";
 
 $message = "<h2>&nbsp;</h2>";
--- viewPurchases.php
+++ viewPurchases.php
@@ -13,8 +13,20 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-include "funcLib.php";
+session_name("WishListSite");
+session_start();
+
+if (!isset($_SESSION["userid"])) {
+    header("Location: login.php");
+    exit;
+}
+
 $userid = $_SESSION["userid"];
+if (isset($_SESSION["euserid"])) {
+    $userid = $_SESSION["euserid"];
+}
+
+include "funcLib.php";
 ?>
 
 <HTML>
