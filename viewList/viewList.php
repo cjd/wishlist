4b7bf649eee89c4c789fc770ee66aa01ddaa58af
Chris Debenham <chris@adebenham.com>
2025-12-05 16:10:34 +1100

    Format tidy

--- viewList/viewList.php
+++ viewList/viewList.php
@@ -19,12 +19,12 @@ $recip = $_REQUEST["recip"];
 $name = $_REQUEST["name"];
 
 $confirm = "";
-if (isset ($_REQUEST["confirm"])) {
+if (isset($_REQUEST["confirm"])) {
     $confirm = $_REQUEST["confirm"];
 }
 
-if($confirm == "Ho Home"){
-     header("Location: " . getFullPath("../home.php"));
+if ($confirm == "Ho Home") {
+    header("Location: " . getFullPath("../home.php"));
 }
 ?>
 <HTML>
@@ -100,46 +100,47 @@ if($confirm == "Ho Home"){
 <title><?php echo $name ?>'s WishList</title>
 
 <?php
-// Alert $userid if new comments have been added to $recip's list since the 
+// Alert $userid if new comments have been added to $recip's list since the
 // last time $userid viewed it (unless $recip == $userid of course)
-$userid=$_SESSION["userid"];
-$query="DELETE FROM comments WHERE date < DATE_SUB(NOW(), INTERVAL 4 MONTH) and userid = '".$recip."'";
-$rs = mysqli_query($link,$query) or die("Could not query: " . mysqli_error($link));
-if($recip != $userid){
-  $query = "select allowEdit from viewList where viewList.viewer = '".$userid."' and viewList.pid='".$recip."'";
-  $rs = mysqli_query($link,$query) or die("Could not query: " . mysqli_error($link));
-  $allowEdit=0;
-  while ($row = mysqli_fetch_assoc($rs)) {
-    if ($row["allowEdit"] == "1") {
-      $userid=$recip;
-      $allowEdit=1;
-      $_SESSION["euserid"]=$recip;
+$userid = $_SESSION["userid"];
+$query = "DELETE FROM comments WHERE date < DATE_SUB(NOW(), INTERVAL 4 MONTH) and userid = '".$recip."'";
+$rs = mysqli_query($link, $query) or die("Could not query: " . mysqli_error($link));
+if ($recip != $userid) {
+    $query = "select allowEdit from viewList where viewList.viewer = '".$userid."' and viewList.pid='".$recip."'";
+    $rs = mysqli_query($link, $query) or die("Could not query: " . mysqli_error($link));
+    $allowEdit = 0;
+    while ($row = mysqli_fetch_assoc($rs)) {
+        if ($row["allowEdit"] == "1") {
+            $userid = $recip;
+            $allowEdit = 1;
+            $_SESSION["euserid"] = $recip;
+        }
     }
-  }
-  $query = "select lastViewDate from comments, viewList where comments.userid=viewList.pid and pid='" . $recip . "' and viewer='" . $userid . "' and comment_userid!='" . $userid . "' and date > lastViewDate";
+    $query = "select lastViewDate from comments, viewList where comments.userid=viewList.pid and pid='" . $recip . "' and viewer='" . $userid . "' and comment_userid!='" . $userid . "' and date > lastViewDate";
 
-  $rs = mysqli_query($link,$query) or die("Could not query: " . mysqli_error($link));
+    $rs = mysqli_query($link, $query) or die("Could not query: " . mysqli_error($link));
 
-  if(mysqli_num_rows($rs) > 0){
-    $alert = "onLoad=\"javascript:alert('New comments have been added since the last time you viewed this list')\"";
-  } else {
-    $alert = "";
-  }
+    if (mysqli_num_rows($rs) > 0) {
+        $alert = "onLoad=\"javascript:alert('New comments have been added since the last time you viewed this list')\"";
+    } else {
+        $alert = "";
+    }
 } else {
-  $allowEdit=1;
+    $allowEdit = 1;
 }
 
-function displayWishlist($recip, $userid, $name, $allowEdit, $link) {
-?>
+function displayWishlist($recip, $userid, $name, $allowEdit, $link)
+{
+    ?>
 <form method="post" action="validate_purchase.php" name="wishlist">
 <input type="hidden" name="receiverUserid" value="<?php echo $recip ?>">
 <input type="hidden" name="receiverName" value="<?php echo $name ?>">
 
 <?php
 
-  printList2($recip, $userid, $name, 0);
+      printList2($recip, $userid, $name, 0);
 
-?>
+    ?>
 
 <p>
 
@@ -156,31 +157,31 @@ function displayWishlist($recip, $userid, $name, $allowEdit, $link) {
 
 <?php
 
-if($recip != $_SESSION["userid"]){
+    if ($recip != $_SESSION["userid"]) {
 
-?>
+        ?>
 <?php
- $query = "select commentId, comment_userid, firstname, lastname, suffix, comment, date from comments, people where comments.userid='" . $recip . "' and comments.comment_userid=people.userid order by date desc";
+        $query = "select commentId, comment_userid, firstname, lastname, suffix, comment, date from comments, people where comments.userid='" . $recip . "' and comments.comment_userid=people.userid order by date desc";
 
-$rs = mysqli_query($link,$query) or die("Could not query: " . mysqli_error($link));
+        $rs = mysqli_query($link, $query) or die("Could not query: " . mysqli_error($link));
 
-while($row = mysqli_fetch_assoc($rs)){
-?>
-<font color="navy"><?php echo $row["firstname"] . ' ' . $row["lastname"] . ' ' . $row["suffix"] ?> made the following comments on <?php echo parseDate($row["date"],1) ?></font>
+        while ($row = mysqli_fetch_assoc($rs)) {
+            ?>
+<font color="navy"><?php echo $row["firstname"] . ' ' . $row["lastname"] . ' ' . $row["suffix"] ?> made the following comments on <?php echo parseDate($row["date"], 1) ?></font>
 <?php
-  if($_SESSION["userid"] == $row["comment_userid"]){
-?>
+              if ($_SESSION["userid"] == $row["comment_userid"]) {
+                  ?>
     <b><a class="menuLink" href="deleteComment.php?commentId=<?php echo $row["commentId"] ?>&recip=<?php echo $recip ?>&name=<?php echo $name ?>"  >[Remove Comment]</a></b>
 <?php
-  }
-?>
+              }
+            ?>
   
 <br>
 <?php echo $row["comment"] ?><p>
 
 <?php
-  }
-?>
+        }
+        ?>
 <form method="post" action="addComment.php">
 <input type="hidden" name="recip" value="<?php echo $recip ?>">
 <input type="hidden" name="name" value="<?php echo $name ?>">
@@ -188,11 +189,10 @@ while($row = mysqli_fetch_assoc($rs)){
 </form>
 
 <?php
-}
-else{
-  print "<font color=red><b>You are not allowed to view comments added to your list</b></font>";
-}
-?>
+    } else {
+        print "<font color=red><b>You are not allowed to view comments added to your list</b></font>";
+    }
+    ?>
 
 </font>
 </td>
@@ -208,18 +208,18 @@ else{
 
 <?php
 
-$query = "select lastModDate from people where userid='" . $recip . "'";
+    $query = "select lastModDate from people where userid='" . $recip . "'";
 
-$rs = mysqli_query($link,$query) or die("Could not query: " . mysqli_error($link));
-   
+    $rs = mysqli_query($link, $query) or die("Could not query: " . mysqli_error($link));
 
-  if($row = mysqli_fetch_assoc($rs)){
-?>
+
+    if ($row = mysqli_fetch_assoc($rs)) {
+        ?>
 <font size=2>
-<font color=indianred><?php echo $name ?></font> last modified this list on <font color=navy><?php echo parseDate($row["lastModDate"],1) ?></font></font>
+<font color=indianred><?php echo $name ?></font> last modified this list on <font color=navy><?php echo parseDate($row["lastModDate"], 1) ?></font></font>
 <?php
-}
-?>
+    }
+    ?>
   
 </td></tr></table>
 </td></tr></table>
@@ -245,7 +245,9 @@ $rs = mysqli_query($link,$query) or die("Could not query: " . mysqli_error($link
 }
 ?>
 
-<BODY <?php if (isset($alert)) {echo $alert;} ?>>
+<BODY <?php if (isset($alert)) {
+    echo $alert;
+} ?>>
 <table class=pagetable>
 <tr>
 <td valign="top">
@@ -262,30 +264,29 @@ createNavBar("../home.php:Home|:View List - " . $_REQUEST["name"], false, "viewO
 <?php
 
   $canEdit = false;
-  $query = "select allowEdit from viewList where pid = '" . $recip . "' and viewer = '" . $_SESSION["userid"] . "'";
-  $rs_edit = mysqli_query($link,$query) or die("Could not query: " . mysqli_error($link));
-  if($row_edit = mysqli_fetch_assoc($rs_edit)){
-    if($row_edit["allowEdit"] == '1'){
-      $canEdit = true;
+$query = "select allowEdit from viewList where pid = '" . $recip . "' and viewer = '" . $_SESSION["userid"] . "'";
+$rs_edit = mysqli_query($link, $query) or die("Could not query: " . mysqli_error($link));
+if ($row_edit = mysqli_fetch_assoc($rs_edit)) {
+    if ($row_edit["allowEdit"] == '1') {
+        $canEdit = true;
     }
-  }
-
-if(($recip == $_SESSION["userid"] || $canEdit) and $confirm == "Edit List"){
-  print "<meta http-equiv=\"refresh\" content=\"0;url=../modifyList/modifyList.php\">";
-  print "</body></html>";
-  return;
 }
-if(($recip == $_SESSION["userid"] || $canEdit) and $confirm != "View List"){
 
-  print "<center><p>&nbsp;<form method=post>";
-  print "<b>Are you sure you want to view this list?<br>You will be able to see any purchases that may have been made!<br>This could ruin the surprise</b>";
-  print "<p><input type=submit name=confirm value=\"View List\" class=\"buttonstyle\"> <input type=submit name=confirm value=\"Edit List\" class=\"buttonstyle\"> <input type=submit name=confirm value=\"Go Home\" class=\"buttonstyle\">";
-  print "</form>\n";
-  print "</center>";
-  print "</td></tr></table></body></html>";
-  return;
+if (($recip == $_SESSION["userid"] || $canEdit) and $confirm == "Edit List") {
+    print "<meta http-equiv=\"refresh\" content=\"0;url=../modifyList/modifyList.php\">";
+    print "</body></html>";
+    return;
 }
-else {
+if (($recip == $_SESSION["userid"] || $canEdit) and $confirm != "View List") {
+
+    print "<center><p>&nbsp;<form method=post>";
+    print "<b>Are you sure you want to view the list of ".$_REQUEST["name"]."?<br>You will be able to see any purchases that may have been made!<br>This could ruin the surprise</b>";
+    print "<p><input type=submit name=confirm value=\"View List\" class=\"buttonstyle\"> <input type=submit name=confirm value=\"Edit List\" class=\"buttonstyle\"> <input type=submit name=confirm value=\"Go Home\" class=\"buttonstyle\">";
+    print "</form>\n";
+    print "</center>";
+    print "</td></tr></table></body></html>";
+    return;
+} else {
     displayWishlist($recip, $userid, $name, $allowEdit, $link);
 }
 ?>
@@ -296,6 +297,6 @@ else {
 
 $query = "update viewList set lastViewDate=NOW() where pid='" . $recip . "' and viewer='" . $userid . "'";
 
-$rs = mysqli_query($link,$query) or die("Could not query: " . mysqli_error($link));
+$rs = mysqli_query($link, $query) or die("Could not query: " . mysqli_error($link));
 
 ?>
